7de4e1316e065ab1adc747ff284760b3fe377176
sambhogal <simranjit.bhogal@gmail.com>
2018-07-14 22:38:07 +1000

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,18 @@
+
+// 
+// var myTag = document.querySelectorAll('td');
+// console.dir(myTag);
+// for (var i = 0; i < myTag.length; i++) {
+//   console.log('***' + myTag[i]);
+// }
+// myTag.addEventListener('click',function() {
+//   alert('hello');
+// });
+//
+// myTag.addEventListener('mouseunclick',function() {
+//   alert('hello');
+// });
+
+
+
+// alert('hello');
